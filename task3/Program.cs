85898aa75fd63301fdbd2219cdb5cba7c772e676
Алексей Нерода <neroda.all@mail.ru>
2023-02-11 11:27:08 +0300

    task3 done

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value % 2 == 0)
+{
+    System.Console.WriteLine("Введенное число является четным");
+}
+else
+{
+    System.Console.WriteLine("Введенное является нечетным");
+}
