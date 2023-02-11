c963444b70db946877aedde54d20bab43982598a
Алексей Нерода <neroda.all@mail.ru>
2023-02-11 11:41:02 +0300

    added description to all task

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,10 @@
-﻿ System.Console.WriteLine("Введите первое число");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+ System.Console.WriteLine("Введите первое число");
  int valueOne = Convert.ToInt32(Console.ReadLine());
  System.Console.WriteLine("Введите второе число");
  int valueTwo = Convert.ToInt32(Console.ReadLine());
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,4 +1,10 @@
-﻿ System.Console.WriteLine("Введите первое число");
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+ 
+ System.Console.WriteLine("Введите первое число");
  int valueOne = Convert.ToInt32(Console.ReadLine());
  System.Console.WriteLine("Введите второе число");
  int valueTwo = Convert.ToInt32(Console.ReadLine());
