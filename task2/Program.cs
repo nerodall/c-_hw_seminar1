2493ce2654e90619240936fb32dbe20d6a995863
Алексей Нерода <neroda.all@mail.ru>
2023-02-11 11:20:53 +0300

    task2 done

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿ System.Console.WriteLine("Введите первое число");
+ int valueOne = Convert.ToInt32(Console.ReadLine());
+ System.Console.WriteLine("Введите второе число");
+ int valueTwo = Convert.ToInt32(Console.ReadLine());
+ System.Console.WriteLine("Введите третье число");
+ int valueThree = Convert.ToInt32(Console.ReadLine());
+
+int max = valueOne;
+
+if (max < valueTwo)
+{
+    max = valueTwo;
+}
+if (max < valueThree)
+{
+    max = valueThree;
+}
+
+System.Console.WriteLine($"Самое больше число из введенных является число {max}");
