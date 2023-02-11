59c5490cf8582881447dc4a57365804888721e24
Алексей Нерода <neroda.all@mail.ru>
2023-02-11 11:07:16 +0300

    task1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ System.Console.WriteLine("Введите первое число");
+ int valueOne = Convert.ToInt32(Console.ReadLine());
+ System.Console.WriteLine("Введите второе число");
+ int valueTwo = Convert.ToInt32(Console.ReadLine());
+
+
+if (valueOne > valueTwo)
+{
+    System.Console.WriteLine($"Число {valueOne} больше числа {valueTwo}");
+}
+else
+{
+    System.Console.WriteLine($"Число {valueTwo} больше числа {valueOne}");
+}
