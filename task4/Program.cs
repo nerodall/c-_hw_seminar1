3ca449783ace6e718944e4605551dcce386862af
Алексей Нерода <neroda.all@mail.ru>
2023-02-11 11:38:14 +0300

    task4 done

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= value) 
+{ 
+    System.Console.Write($"{count} ");
+    count += 2;
+}
